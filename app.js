1701f568a29a2717cfdfbbab3a965999e6cb983a
Juan Cabello <jcabelloc@itana.pe>
2024-10-25 19:27:48 -0500

    Conexion a la BD MongoDB

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
+const database = require('./utils/database');
+const mongoConnect = database.mongoConnect;
 
 const adminRoutes = require('./routes/admin')
 const tiendaRoutes = require('./routes/tienda')
 const errorController = require('./controllers/error');
-const sequelize = require('./utils/database');
-const Producto = require('./models/producto');
-const Usuario = require('./models/usuario');
-const Carrito = require('./models/carrito');
-const CarritoItem = require('./models/carritoitem');
-const Pedido = require('./models/pedido');
-const PedidoItem = require('./models/pedidoitem');
 
 const app = express();
 
@@ -25,54 +21,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    Usuario.findByPk(1)
-        .then(usuario => {
-            req.usuario = usuario;
-            next();
-        })
-        .catch(err => console.log(err));
+    next();
 })
 
 app.use('/admin', adminRoutes);
 app.use(tiendaRoutes);
 
-app.use((req, res, next) => {
-    next();
-});
 
 app.use(errorController.get404);
 
-Producto.belongsTo(Usuario, { constraints: true, onDelete: 'CASCADE' });
-Usuario.hasMany(Producto);
-Usuario.hasOne(Carrito);
-Carrito.belongsTo(Usuario);
-Carrito.belongsToMany(Producto, { through: CarritoItem });
-Producto.belongsToMany(Carrito, { through: CarritoItem });
+mongoConnect(() => {
+    app.listen(3000);
+})
 
-Pedido.belongsTo(Usuario);
-Usuario.hasMany(Pedido);
-Pedido.belongsToMany(Producto, { through: PedidoItem });
-
-
-
-sequelize
-    //.sync()
-    .sync({ force: true }) // Durante Desarrollo
-    .then(result => {
-        return Usuario.findByPk(1);
-    })
-    .then(usuario => {
-        if (!usuario) {
-            // Crear el usuario
-            return Usuario.create({ nombre: 'Juan', email: 'juan@gmail.com' });
-        }
-        return usuario
-    })
-    .then(usuario => {
-        return usuario.createCarrito();
-    })
-    .then(carrito => {
-        app.listen(3000);
-    })
-    .catch(err => console.log(err));
 
